d51e820ff48278d66a5cb304c593aa213a774d05
Максим Овчинников <ovchinnikov.mv@reliab.tech>
2021-01-21 14:29:36 +0300

    Подправил вид кода.

--- src/main/kotlin/bTree.kt
+++ src/main/kotlin/bTree.kt
@@ -1,6 +1,6 @@
 import java.util.*
 
-class BTree(private val t: Int) {
+class BTree(private val parameter: Int) {
     private val node: BNode?
 
     init {
@@ -17,7 +17,7 @@ class BTree(private val t: Int) {
             return sum + children.size
         }
 
-        fun add_child(child: BNode) {
+        private fun add_child(child: BNode) {
             children.add(child)
             child.parent = this
         }
@@ -28,7 +28,7 @@ class BTree(private val t: Int) {
             keys = mutableListOf()
         }
 
-        fun delete_child(child: BNode?) {
+        private fun delete_child(child: BNode?) {
             children.remove(child)
         }
 
@@ -41,8 +41,8 @@ class BTree(private val t: Int) {
             keys.remove(value!!)
         }
 
-        fun restruct() {
-            if (keys.size != 2 * t - 1) return
+        private fun restruct() {
+            if (keys.size != 2 * parameter - 1) return
             val first_child = BNode(this)
             val second_child = BNode(this)
             for (i in 0 until children.size / 2) {
@@ -54,7 +54,7 @@ class BTree(private val t: Int) {
                 second_child.insert_key(keys[(keys.size + 1) / 2])
             }
             if (parent == null) {
-                val key = keys[t - 1]
+                val key = keys[parameter - 1]
                 keys = ArrayList()
                 keys.add(key)
                 children.add(first_child)
@@ -84,7 +84,7 @@ class BTree(private val t: Int) {
                     }
                     i++
                 }
-                if (!flag && keys.size < 2 * t - 1) {
+                if (!flag && keys.size < 2 * parameter - 1) {
                     insert_key(value)
                     flag = true
                 }
@@ -124,16 +124,16 @@ class BTree(private val t: Int) {
         var search_node = node!!
         var flag = false
         var index_node = 0
-        if (search_node.keys.get(search_node.keys.size - 1) >= key) {
+        if (search_node.keys[search_node.keys.size - 1] >= key) {
             var i = 0
             while (i < search_node.keys.size && !flag) {
-                if (key == search_node.keys.get(i)) flag = true
+                if (key == search_node.keys[i]) flag = true
                 i++
             }
         }
         while (!flag) {
             for (child in search_node.children) {
-                if (child.keys.get(child.keys.size - 1)
+                if (child.keys[child.keys.size - 1]
                     >= key
                 ) {
                     for (value in child.keys) {
@@ -146,12 +146,12 @@ class BTree(private val t: Int) {
                     break
                 }
             }
-            search_node = if (search_node.children.size != 0) search_node.children.get(index_node) else break
+            search_node = if (search_node.children.size != 0) search_node.children[index_node] else break
         }
         return flag
     }
 
-    fun delete_node(bnode: BNode, key: Int) {
+    private fun delete_node(bnode: BNode, key: Int) {
         var left_right = 0
         var flag = false
         if (bnode.children.size == 0) {
@@ -159,35 +159,35 @@ class BTree(private val t: Int) {
             var key_del = -1
             var i = 1
             while (i < bnode.parent!!.keys.size && key_del == -1) {
-                if (key > bnode.parent!!.keys.get(i)) key_del = bnode.parent!!.keys.get(i)
+                if (key > bnode.parent!!.keys[i]) key_del = bnode.parent!!.keys[i]
                 i++
             }
             if (bnode.parent!!.children.indexOf(bnode) != 0) {
-                bNodeNeighbor = bnode.parent!!.children.get(bnode.parent!!.children.indexOf(bnode) - 1)
-                if (bNodeNeighbor.keys.size > t - 1) {
+                bNodeNeighbor = bnode.parent!!.children[bnode.parent!!.children.indexOf(bnode) - 1]
+                if (bNodeNeighbor.keys.size > parameter - 1) {
                     bnode.parent!!.delete_key(key_del)
                     bnode.insert_key(key_del)
-                    bnode.parent!!.insert_key(bNodeNeighbor.keys.get(bNodeNeighbor.keys.size - 1))
-                    bNodeNeighbor.delete_key(bNodeNeighbor.keys.get(bNodeNeighbor.keys.size - 1))
+                    bnode.parent!!.insert_key(bNodeNeighbor.keys[bNodeNeighbor.keys.size - 1])
+                    bNodeNeighbor.delete_key(bNodeNeighbor.keys[bNodeNeighbor.keys.size - 1])
                     flag = true
                 } else left_right = -1
             } else if (bnode.parent!!.children.indexOf(bnode) != bnode.parent!!
                     .children.size - 1 && !flag
             ) {
-                bNodeNeighbor = bnode.parent!!.children.get(bnode.parent!!.children.indexOf(bnode) + 1)
-                if (bNodeNeighbor.keys.size > t - 1) {
+                bNodeNeighbor = bnode.parent!!.children[bnode.parent!!.children.indexOf(bnode) + 1]
+                if (bNodeNeighbor.keys.size > parameter - 1) {
                     bnode.parent!!.delete_key(key_del)
                     bnode.insert_key(key_del)
-                    bnode.parent!!.insert_key(bNodeNeighbor.keys.get(0))
-                    bNodeNeighbor.delete_key(bNodeNeighbor.keys.get(0))
+                    bnode.parent!!.insert_key(bNodeNeighbor.keys[0])
+                    bNodeNeighbor.delete_key(bNodeNeighbor.keys[0])
                     flag = true
                 } else left_right = 1
             } else if (!flag) {
                 for (value in bnode.keys) {
-                    bnode.parent!!.children.get(bnode.parent!!.children.indexOf(bnode) + left_right)
+                    bnode.parent!!.children[bnode.parent!!.children.indexOf(bnode) + left_right]
                         .insert_key(value)
                 }
-                bnode.parent!!.children.get(bnode.parent!!.children.indexOf(bnode) + left_right)
+                bnode.parent!!.children[bnode.parent!!.children.indexOf(bnode) + left_right]
                     .insert_key(key_del)
                 bnode.parent!!.delete_key(key_del)
                 bnode.remove()
@@ -196,28 +196,32 @@ class BTree(private val t: Int) {
             var right_child = bnode
             var left_child = bnode
             for (i in bnode.children.indices) {
-                if (key > bnode.children.get(i).keys.get(0)) {
-                    right_child = bnode.children.get(i)
-                    left_child = bnode.children.get(i - 1)
+                if (key > bnode.children[i].keys[0]) {
+                    right_child = bnode.children[i]
+                    left_child = bnode.children[i - 1]
                     break
                 }
             }
-            if (left_child.keys.size > t - 1) {
-                val child_key: Int = left_child.keys.get(left_child.keys.size - 1)
-                bnode.insert_key(child_key)
-                left_child.delete_key(child_key)
-                delete_node(left_child, child_key)
-            } else if (right_child.keys.size > t - 1) {
-                val child_key: Int = right_child.keys.get(0)
-                bnode.insert_key(child_key)
-                right_child.delete_key(child_key)
-                delete_node(right_child, child_key)
-            } else {
-                for (value in right_child.keys) {
-                    left_child.insert_key(value!!)
+            when {
+                left_child.keys.size > parameter - 1 -> {
+                    val child_key: Int = left_child.keys.get(left_child.keys.size - 1)
+                    bnode.insert_key(child_key)
+                    left_child.delete_key(child_key)
+                    delete_node(left_child, child_key)
                 }
-                right_child.remove()
-                delete_node(left_child, key)
+                right_child.keys.size > parameter - 1 -> {
+                    val child_key: Int = right_child.keys.get(0)
+                    bnode.insert_key(child_key)
+                    right_child.delete_key(child_key)
+                    delete_node(right_child, child_key)
+                }
+                else -> {
+                    for (value in right_child.keys) {
+                        left_child.insert_key(value)
+                    }
+                    right_child.remove()
+                    delete_node(left_child, key)
+                }
             }
         }
     }
@@ -225,22 +229,22 @@ class BTree(private val t: Int) {
     fun delete(key: Int) {
         var search_node = node!!
         var index_node = 0
-        if (search_node.keys.get(search_node.keys.size - 1) >= key) {
-            for (i in search_node.keys.indices) if (key == search_node.keys.get(i)) {
+        if (search_node.keys[search_node.keys.size - 1] >= key) {
+            for (i in search_node.keys.indices) if (key == search_node.keys[i]) {
                 search_node.delete_key(key)
-                if (search_node.keys.size <= t - 1) delete_node(search_node, key)
+                if (search_node.keys.size <= parameter - 1) delete_node(search_node, key)
                 return
             }
         }
         while (true) {
             for (child in search_node.children) {
-                if (child.keys.get(child.keys.size - 1)
+                if (child.keys[child.keys.size - 1]
                     >= key
                 ) {
                     for (value in child.keys) {
                         if (key == value) {
                             child.delete_key(key)
-                            if (child.keys.size <= t - 1) delete_node(child, key)
+                            if (child.keys.size <= parameter - 1) delete_node(child, key)
                             return
                         }
                     }
@@ -248,7 +252,7 @@ class BTree(private val t: Int) {
                     break
                 }
             }
-            search_node = if (search_node.children.size != 0) search_node.children.get(index_node) else break
+            search_node = if (search_node.children.size != 0) search_node.children[index_node] else break
         }
     }
 
